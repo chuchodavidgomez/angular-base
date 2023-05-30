23703b44bc992904a3984444e5144380780958d2
Jesus David Gomez Osorno <jesus.gomez@ceiba.com.co>
2023-05-30 16:28:52 -0500

    se ajusto scripts

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 import { DbzService } from '../services/dbz.service';
 
